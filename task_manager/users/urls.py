a2f0cdc72976918c1d7eb3b7a854c08ad0899810
Mikhail Orlov <crazyminer398@gmail.com>
2023-02-28 00:19:45 +0300

    added user CRUD

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -4,9 +4,9 @@ from django.urls import path
 
 urlpatterns = [
     path('users/', views.UserListView.as_view(), name='user-list'),
-    path('users/create/', views.UserCreateView.as_view(), name='create'),
+    path('users/create/', views.UserCreateView.as_view(), name='register'),
     path('users/<int:pk>/update/', views.UserUpdateView.as_view(), name='update'),
     path('users/<int:pk>/delete/', views.UserDeleteView.as_view(), name='delete'),
-    # path('login/', views.LoginUserView.as_view(), name='login'),
-    # path('logout/', views.LogoutUsersView.as_view(), name='logout'),
+    path('login/', views.LoginUserView.as_view(), name='login'),
+    path('logout/', views.LogoutUsersView.as_view(), name='logout'),
 ]
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,18 +1,27 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from .models import User
+from .forms import NewUserCreationForm
 
 
 class UserListView(ListView):
     model = get_user_model()
-    template_name = 'users/users.html'
+    template_name = 'users/show.html'
     context_object_name = 'users'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
 
 
 class UserCreateView(CreateView):
-    model = User
+    form_class = NewUserCreationForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('login')
 
 
 class UserUpdateView(UpdateView):
@@ -21,3 +30,11 @@ class UserUpdateView(UpdateView):
 
 class UserDeleteView(DeleteView):
     model = User
+
+
+class LoginUserView(LoginView):
+    template_name = 'users/login.html'
+
+
+class LogoutUsersView(LogoutView):
+    pass
