34ea94aaf58e5f41dcfc8058ca0e66c089b73526
Un1que11 <crazyminer398@gmail.com>
2023-03-23 19:15:00 +0300

    added new url

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', views.Index.as_view()),
+    path('', views.Index.as_view(), name='home'),
     path('', include('task_manager.users.urls')),
     path('admin/', admin.site.urls),
 ]
