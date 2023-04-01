28b2096ecc884803f5afb0e5150c3c9ae69d829b
Un1que11 <crazyminer398@gmail.com>
2023-04-02 00:13:01 +0300

    fixed bugs

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -67,10 +67,10 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(
-        conn_max_age=60
-        )
-DATABASES['default'].update(db_from_env)
+if not DEBUG:
+    DATABASES['default'] = dj_database_url.config(
+            conn_max_age=600
+            )
 
 
 AUTH_PASSWORD_VALIDATORS = [
